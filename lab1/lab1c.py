7a2ba1790b7a6d26b0f814310c10422bc1648dcb
Raymond Domingo <rdomingo6@myseneca.ca>
2024-06-01 13:25:43 -0400

    added lab1 script and output text

---
+++ lab1/lab1c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+name = 'Isaac'
+age = 72
+
+print('Isaac is ' + str(age) + ' years old!')
---
+++ lab1/lab1d.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+x = 10
+y = 2
+z = 5
+result = x + y * 5
+print('10 + 2 * 5 = ' + str(result))
