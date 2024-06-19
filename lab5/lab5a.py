e3d87f40e02c79a626fb54f2ee45da1edfd6bc36
Raymond Domingo <rdomingo6@myseneca.ca>
2024-06-19 00:17:17 -0400

    lab 5 done

---
+++ lab5/lab5a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# Author ID: [seneca_id]
+
+def read_file_string(file_name):
+    # Takes file_name as string for a file name, returns its entire contents as a string
+    f = open(file_name, 'r')
+    read_data = f.read()
+    f.close()
+    return read_data
+
+def read_file_list(file_name):
+    # Takes a file_name as string for a file name, 
+    # return its entire contents as a list of lines without new-line characters
+    f = open(file_name, 'r')
+    read_data = f.readlines()
+    f.close()
+    new_list = []
+    for item in read_data:
+        if item[-1] == '\n':
+            new_list.append(item[:-1])
+        else:
+            new_list.append(item)
+    return new_list
+    
+    
+
+if __name__ == '__main__':
+    file_name = 'data.txt'
+    print(read_file_string(file_name))
+    print(read_file_list(file_name))
---
+++ lab5/lab5b.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# Author ID: [seneca_id]
+
+def read_file_string(file_name):
+    # Takes file_name as string for a file name, returns its entire contents as a string
+    f = open(file_name, 'r')
+    read_data = f.read()
+    f.close()
+    return read_data
+
+def read_file_list(file_name):
+    # Takes a file_name as string for a file name, 
+    # return its entire contents as a list of lines without new-line characters
+    f = open(file_name, 'r')
+    read_data = f.readlines()
+    f.close()
+    new_list = []
+    for item in read_data:
+        if item[-1] == '\n':
+            new_list.append(item[:-1])
+        else:
+            new_list.append(item)
+    return new_list
+    
+def append_file_string(file_name, string_of_lines):
+    # Takes two strings, appends the string to the end of the file
+    f = open(file_name, 'a')
+    f.write(string_of_lines)
+    f.close()
+
+def write_file_list(file_name, list_of_lines):
+    # Takes a string and list, writes all items from list to file where each item is one line
+    f = open(file_name, 'w')
+    for item in list_of_lines:
+        f.write(str(item) + '\n')
+    f.close()
+
+def copy_file_add_line_numbers(file_name_read, file_name_write):
+    # Takes two strings, reads data from first file, writes data to new file, adds line number to new file
+    f = open(file_name_write, 'w')
+    f2 = open(file_name_read, 'r')
+    n = 1
+    for item in f2:
+        f.write(str(n)+':'+item)
+        n = n+1
+    f.close()
+    f2.close()
+
+if __name__ == '__main__':
+    file1 = 'seneca1.txt'
+    file2 = 'seneca2.txt'
+    file3 = 'seneca3.txt'
+    string1 = 'First Line\nSecond Line\nThird Line\n'
+    list1 = ['Line 1', 'Line 2', 'Line 3']
+    append_file_string(file1, string1)
+    print(read_file_string(file1))
+    write_file_list(file2, list1)
+    print(read_file_string(file2))
+    copy_file_add_line_numbers(file2, file3)
+    print(read_file_string(file3))
